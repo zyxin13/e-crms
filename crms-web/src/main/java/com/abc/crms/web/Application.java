d728fbaf4d2d2054f3515c5ec9bbfd91e4162af1
zyxin <337902923@qq.com>
2017-03-13 01:58:48 +0800

    1. 初始化项目，创建crms-web模块

---
+++ crms-web/src/main/java/com/abc/crms/web/Application.java
@@ -0,0 +1,14 @@
+package com.abc.crms.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 邹玉鑫 on 2017/3/13.
+ */
+@SpringBootApplication
+public class Application{
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ crms-web/src/main/java/com/abc/crms/web/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.abc.crms.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 邹玉鑫 on 2017/3/13.
+ */
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
